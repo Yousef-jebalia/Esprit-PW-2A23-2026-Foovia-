ad074e840c5670d678045707ea3494858bda6063
Youssef Jebalia <yousefjebalia@gmail.com>
2026-04-24 22:00:07 +0100

    Enhanced front office design and implemnted anime.js animation

--- MVC/View/back_office/add_traitement_page.php
+++ MVC/View/back_office/add_traitement_page.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__ . '/../../../MVC/Controller/Traitemant_Controller.php';
+include __DIR__ . '/../../MVC/Controller/Traitemant_Controller.php';
 
 $error = '';
 $success = '';
@@ -46,6 +46,8 @@ $editMode = false;
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
